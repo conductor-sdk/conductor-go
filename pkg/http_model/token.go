84461b719c2ccea1fd501e24c354df393c84f7a2
gardusig <gustavo.gardusi@gmail.com>
2022-04-15 10:31:11 -0300

    Add token to HTTP models

--- pkg/http_model/token.go
+++ pkg/http_model/token.go
@@ -1,13 +1,5 @@
 package http_model
 
-import "encoding/json"
-
 type Token struct {
 	Token string `json:"token"`
-}
-
-func GetTokenFromResponse(response string) (*Token, error) {
-	t := new(Token)
-	err := json.Unmarshal([]byte(response), t)
-	return t, err
 }
