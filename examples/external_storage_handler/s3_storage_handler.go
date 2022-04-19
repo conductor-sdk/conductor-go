0f8be3dd39679469fa9d40db16a1e2b16e7869ee
gardusig <gustavo.gardusi@gmail.com>
2022-04-18 22:38:46 -0300

    Refactor code and documentation in order to work with go pkg index

--- examples/external_storage_handler/s3_storage_handler.go
+++
@@ -1,5 +0,0 @@
-package external_storage_handler
-
-func S3UploadAndGetPath(data map[string]interface{}) (string, error) {
-	return "RANDOMPATH", nil
-}
---
+++ examples/external_storage_handler/simple_storage_handler.go
@@ -0,0 +1,5 @@
+package external_storage_handler
+
+func UploadAndGetPath(data map[string]interface{}) (string, error) {
+	return "PATH", nil
+}
