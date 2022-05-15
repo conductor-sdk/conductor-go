04e3fc2346a1d7526b72dbe9ab9c5aef33fd28af
gardusig <gustavo.gardusi@gmail.com>
2022-05-15 13:20:08 -0300

    Fix issues with fork join

--- pkg/workflow/def/workflow/fork_join.go
+++ pkg/workflow/def/workflow/fork_join.go
@@ -1,63 +1,43 @@
 package workflow
 
-// import "github.com/conductor-sdk/conductor-go/pkg/http_model"
+import "github.com/conductor-sdk/conductor-go/pkg/http_model"
 
-// func Fork(taskRefName string, tasks ...[]Task) *fork {
-// 	return &fork{
-// 		task: task{
-// 			name:              taskRefName,
-// 			taskReferenceName: taskRefName,
-// 			description:       "",
-// 			taskType:          FORK_JOIN,
-// 			optional:          false,
-// 			inputParameters:   map[string]interface{}{},
-// 		},
-// 		forkedTasks: tasks,
-// 	}
-// }
+func Fork(taskRefName string, tasks ...[]Task) *fork {
+	return &fork{
+		task: task{
+			name:              taskRefName,
+			taskReferenceName: taskRefName,
+			description:       "",
+			taskType:          FORK_JOIN,
+			optional:          false,
+			inputParameters:   map[string]interface{}{},
+		},
+		forkedTasks: tasks,
+	}
+}
 
-// type fork struct {
-// 	task
-// 	forkedTasks [][]Task
-// 	joinTask    Task
-// }
+type fork struct {
+	task
+	forkedTasks [][]Task
+	joinTask    Task
+}
 
-// func (task *fork) toWorkflowTask() []http_model.WorkflowTask {
-// 	workflowTask := task.task.toWorkflowTask()[0]
-// 	workflowTask.ForkTasks = task.forkedTasks
+func (task *fork) toWorkflowTask() []http_model.WorkflowTask {
+	forkWorkflowTask := task.task.toWorkflowTask()[0]
+	forkWorkflowTask.ForkTasks = make([][]http_model.WorkflowTask, len(task.forkedTasks))
+	for i, forkedTask := range task.forkedTasks {
+		forkWorkflowTask.ForkTasks[i] = make([]http_model.WorkflowTask, len(forkedTask))
+		for j, innerForkedTask := range forkedTask {
+			forkWorkflowTask.ForkTasks[i][j] = innerForkedTask.toWorkflowTask()[0]
+		}
+	}
+	return []http_model.WorkflowTask{
+		forkWorkflowTask,
+		task.joinTask.toWorkflowTask()[0],
+	}
+}
 
-// 	for _, forkedTask := range task.forkedTasks {
-// 		for _, innerForkedTask := range forkedTask {
-// 			workflowTask.ForkTasks  = append(
-// 				workflowTasks.forkedTask,
-// 				innerForkedTask,
-// 			)
-// 		}
-// 	}
-// 	workflowTask := task.task.toWorkflowTask()[0]
-// 	for _, forkedTask := range task.forkedTasks {
-// 		var forkedTasksInner []http_model.WorkflowTask
-// 		for _, t := range forkedTask {
-// 			forkedTasksInner = append(
-// 				forkedTasksInner,
-// 				t.toWorkflowTask()...,
-// 			)
-// 		}
-// 		workflowTask.ForkTasks = append(
-// 			workflowTask.ForkTasks,
-// 			forkedTasksInner,
-// 		)
-// 	}
-// 	if task.joinTask != nil {
-// 		workflowTasks = append(
-// 			workflowTasks,
-// 			task.joinTask.toWorkflowTask()...,
-// 		)
-// 	}
-// 	return workflowTasks
-// }
-
-// func (task *fork) Input(key string, value interface{}) *fork {
-// 	task.task.Input(key, value)
-// 	return task
-// }
+func (task *fork) Input(key string, value interface{}) *fork {
+	task.task.Input(key, value)
+	return task
+}
