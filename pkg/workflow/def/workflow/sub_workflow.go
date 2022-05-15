143f1511fad62da87719e43227713e795bcb53ee
Viren Baraiya <virenx@gmail.com>
2022-05-15 10:41:02 -0700

    Update sub_workflow.go

--- pkg/workflow/def/workflow/sub_workflow.go
+++ pkg/workflow/def/workflow/sub_workflow.go
@@ -17,11 +17,26 @@ func SubWorkflow(taskRefName string, workflowName string, version *int32) *subWo
 	}
 }
 
+func SubWorkflowInline(taskRefName string, workflow *conductorWorkflow) *subWorkflow {
+	return &subWorkflow{
+		task: task{
+			name:              taskRefName,
+			taskReferenceName: taskRefName,
+			description:       "",
+			taskType:          SUB_WORKFLOW,
+			optional:          false,
+			inputParameters:   map[string]interface{}{},
+		},
+		workflow: workflow,
+	}
+}
+
 type subWorkflow struct {
 	task
 	workflowName    string
 	version         *int32
 	taskToDomainMap map[string]string
+	workflow        *conductorWorkflow
 }
 
 func (task *subWorkflow) Description(description string) *subWorkflow {
@@ -43,11 +58,19 @@ func (task *subWorkflow) TaskToDomain(taskToDomainMap map[string]string) *subWor
 }
 func (task *subWorkflow) toWorkflowTask() []http_model.WorkflowTask {
 	workflowTasks := task.task.toWorkflowTask()
-	workflowTasks[0].SubWorkflowParam = &http_model.SubWorkflowParams{
-		Name:               task.workflowName,
-		Version:            task.version,
-		TaskToDomain:       task.taskToDomainMap,
-		WorkflowDefinition: nil,
+	if task.workflow != nil {
+		workflowTasks[0].SubWorkflowParam = &http_model.SubWorkflowParams{
+			Name:               task.workflow.name,
+			TaskToDomain:       task.taskToDomainMap,
+			WorkflowDefinition: task.workflow.toWorkflowDef(),
+		}
+	} else {
+		workflowTasks[0].SubWorkflowParam = &http_model.SubWorkflowParams{
+			Name:               task.workflowName,
+			Version:            task.version,
+			TaskToDomain:       task.taskToDomainMap,
+			WorkflowDefinition: nil,
+		}
 	}
 	return workflowTasks
 }
