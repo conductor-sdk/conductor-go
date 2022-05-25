ee27b1b870c30c706c01f094baf680c2eb2ebea8
gardusig <gustavo.gardusi@gmail.com>
2022-05-25 15:19:09 -0300

    Minor refactor for panic recover

--- pkg/concurrency/recover.go
+++ pkg/concurrency/recover.go
@@ -2,16 +2,18 @@ package concurrency
 
 import (
 	"github.com/conductor-sdk/conductor-go/pkg/metrics/metrics_counter"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-func OnError(message string) {
-	if err := recover(); err != nil {
-		metrics_counter.IncrementUncaughtException(message)
-		log.Error(
-			"Uncaught error",
-			", message: ", message,
-			", error: ", err,
-		)
+func HandlePanicError(message string) {
+	err := recover()
+	if err == nil {
+		return
 	}
+	metrics_counter.IncrementUncaughtException(message)
+	logrus.Warning(
+		"Uncaught panic",
+		", message: ", message,
+		", error: ", err,
+	)
 }
