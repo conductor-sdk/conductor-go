f3d5cef571ca538b595747cba37b84e890e0c2ab
gardusig <gustavo.gardusi@gmail.com>
2022-06-01 23:59:29 -0300

    Supress verbose log message for workflow monitor

--- pkg/workflow/executor/workflow_monitor.go
+++ pkg/workflow/executor/workflow_monitor.go
@@ -126,7 +126,7 @@ func (w *WorkflowMonitor) getRunningWorkflowIdList() ([]string, error) {
 func (w *WorkflowMonitor) notifyFinishedWorkflow(workflowId string, workflow *http_model.Workflow) error {
 	w.mutex.Lock()
 	defer w.mutex.Unlock()
-	log.Debug(fmt.Sprintf("Notifying finished workflow: %+v", *workflow))
+	log.Debug(fmt.Sprintf("Notifying finished workflowId: %s", workflowId))
 	executionChannel, ok := w.executionChannelByWorkflowId[workflowId]
 	if !ok {
 		return fmt.Errorf("execution channel not found for workflowId: %s", workflowId)
