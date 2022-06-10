f49100a01298e2fc70dfbff6a3a9d9822e48996e
gardusig <gustavo.gardusi@gmail.com>
2022-06-09 22:19:56 -0300

    Minor improvements to shipment example

--- examples/shipment_example/workflow.go
+++ examples/shipment_example/workflow.go
@@ -1,1 +1,14 @@
 package shipment_example
+
+import (
+	"github.com/conductor-sdk/conductor-go/pkg/workflow/def/workflow"
+	"github.com/conductor-sdk/conductor-go/pkg/workflow/executor"
+)
+
+func NewShipmentWorkflow(workflowExecutor *executor.WorkflowExecutor) *workflow.ConductorWorkflow {
+	return workflow.NewConductorWorkflow(workflowExecutor).
+		Name("EXAMPLE_GO_SHIPMENT_WORKFLOW").
+		Version(1).
+		OwnerEmail("developers@orkes.io").
+		TimeoutPolicy(workflow.TimeOutWorkflow, 60)
+}
