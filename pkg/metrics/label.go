a30ac5405f75c02fe6a58a60ba8f2884cb52ca92
Viren Baraiya <virenx@gmail.com>
2022-06-12 17:09:41 -0700

    Update label.go

--- pkg/metrics/label.go
+++ pkg/metrics/label.go
@@ -4,10 +4,10 @@ type MetricLabel string
 
 const (
 	ENTITY_NAME      MetricLabel = "entityName"
-	EXCEPTION                    = "exception"
-	OPERATION                    = "operation"
-	PAYLOAD_TYPE                 = "payload_type"
-	TASK_TYPE                    = "taskType"
-	WORKFLOW_TYPE                = "workflowType"
-	WORKFLOW_VERSION             = "version"
+	EXCEPTION        MetricLabel = "exception"
+	OPERATION        MetricLabel = "operation"
+	PAYLOAD_TYPE     MetricLabel = "payload_type"
+	TASK_TYPE        MetricLabel = "taskType"
+	WORKFLOW_TYPE    MetricLabel = "workflowType"
+	WORKFLOW_VERSION MetricLabel = "version"
 )
