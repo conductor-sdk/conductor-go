8c1fbe58837f07054950d53c70a8aac33c1dab8b
manan164 <1897158+manan164@users.noreply.github.com>
2023-12-07 20:56:43 +0530

    test

---
+++ test/integration_tests/workflow_idempotency_test.go
@@ -0,0 +1,48 @@
+package integration_tests
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/conductor-sdk/conductor-go/internal/testdata"
+	"github.com/conductor-sdk/conductor-go/sdk/model"
+	"github.com/conductor-sdk/conductor-go/sdk/workflow"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIdempotencyCombinations(t *testing.T) {
+	executor := testdata.WorkflowExecutor
+	wf := workflow.NewConductorWorkflow(executor)
+	wf.Name("temp_wf_" + strconv.Itoa(time.Now().Nanosecond())).Version(1)
+	wf = wf.Add(workflow.NewSetVariableTask("set_var").Input("var_value", 42))
+	err := wf.Register(true)
+
+	assert.NoError(t, err, "Failed to register workflow")
+
+	id, olderr := executor.StartWorkflow(&model.StartWorkflowRequest{Name: wf.GetName(), IdempotencyKey: "test", IdempotencyStrategy: "FAIL"})
+	assert.NoError(t, olderr, "Failed to start workflow")
+
+	oldid, err := executor.StartWorkflow(&model.StartWorkflowRequest{Name: wf.GetName(), IdempotencyKey: "test", IdempotencyStrategy: "FAIL"})
+	assert.Error(t, err, "Failed to start workflow")
+	assert.Error(t, err, oldid)
+
+	execution, err := executor.GetWorkflow(id, true)
+	assert.NoError(t, err, "Failed to get workflow execution")
+	assert.Equal(t, model.CompletedWorkflow, execution.Status, "Workflow is not in the completed state")
+
+	err = executor.RemoveWorkflow(id)
+	assert.NoError(t, err, "Failed to remove workflow execution")
+
+	_, err = executor.GetWorkflow(id, true)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "no such workflow by Id")
+
+	_, err = testdata.MetadataClient.UnregisterWorkflowDef(
+		context.Background(),
+		wf.GetName(),
+		wf.GetVersion(),
+	)
+	assert.NoError(t, err, "Failed to delete workflow definition ", err)
+}
