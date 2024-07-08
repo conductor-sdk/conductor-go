c686ddf0dffc10c171909895732466640917eeba
Miguel Prieto <miguel.prieto@orkes.io>
2024-07-08 19:48:10 -0400

    Added a \n to avoid having the license in the package documentation as here: https://pkg.go.dev/github.com/conductor-sdk/conductor-go@v1.4.0/sdk/model/rbac

--- sdk/client/api_group_resource.go
+++ sdk/client/api_group_resource.go
@@ -6,15 +6,17 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
+
 package client
 
 import (
 	"context"
 	"fmt"
-	"github.com/conductor-sdk/conductor-go/sdk/model/rbac"
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/conductor-sdk/conductor-go/sdk/model/rbac"
 )
 
 type GroupResourceApiService struct {
