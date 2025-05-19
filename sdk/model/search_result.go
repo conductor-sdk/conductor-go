e048bd644e7b9b5adc375c6bd9c83ee3435ed478
Shailesh Jagannath Padave <shaileshpadave@192.168.1.4>
2025-05-19 17:49:35 +0530

    added SearchResult

--- sdk/model/search_result.go
+++ sdk/model/search_result.go
@@ -7,6 +7,7 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-package security
+package model
 
-type ConfigKey struct {}
+type SearchResult struct {
+}
