0e1c520bf5c3de67c98e22a1dcf6a2b5b5db406d
Shailesh Padave <shaileshpadave49@gmail.com>
2025-05-19 23:45:18 +0530

    Update SDK POJO (github.com/conductor-sdk/conductor-go/sdk/model.start_workflow_request) to match server implementation

--- sdk/model/start_workflow_request.go
+++ sdk/model/start_workflow_request.go
@@ -13,41 +13,12 @@ type StartWorkflowRequest struct {
 	Name                            string              `json:"name"`
 	Version                         int32               `json:"version,omitempty"`
 	CorrelationId                   string              `json:"correlationId,omitempty"`
-	Input                           interface{}         `json:"input,omitempty"`
+	Input                           map[string]interface{} `json:"input,omitempty"`
 	TaskToDomain                    map[string]string   `json:"taskToDomain,omitempty"`
 	WorkflowDef                     *WorkflowDef        `json:"workflowDef,omitempty"`
 	ExternalInputPayloadStoragePath string              `json:"externalInputPayloadStoragePath,omitempty"`
 	Priority                        int32               `json:"priority,omitempty"`
+	CreatedBy                       string              `json:"createdBy,omitempty"`
 	IdempotencyKey                  string              `json:"idempotencyKey,omitempty"`
 	IdempotencyStrategy             IdempotencyStrategy `json:"idempotencyStrategy,omitempty"`
-}
-
-func NewStartWorkflowRequest(name string, version int32, correlationId string, input interface{}) *StartWorkflowRequest {
-	return &StartWorkflowRequest{
-		Name:          name,
-		Version:       version,
-		CorrelationId: correlationId,
-		Input:         input,
-	}
-}
-
-func NewIdempotentStartWorkflowRequest(name string, version int32, correlationId string,
-	idempotencyKey string, idempotencyStrategy IdempotencyStrategy, input interface{}) *StartWorkflowRequest {
-	return &StartWorkflowRequest{
-		Name:                name,
-		Version:             version,
-		CorrelationId:       correlationId,
-		Input:               input,
-		IdempotencyKey:      idempotencyKey,
-		IdempotencyStrategy: idempotencyStrategy,
-	}
-}
-
-func NewStartWorkflowRequestLegacy(name string, version int32, correlationId string, input interface{}) *StartWorkflowRequest {
-	return &StartWorkflowRequest{
-		Name:          name,
-		Version:       version,
-		CorrelationId: correlationId,
-		Input:         input,
-	}
 }
