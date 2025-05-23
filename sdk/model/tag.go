7c1a8d969503a3ada9b56dabf07053929f9a98fa
Shailesh Jagannath Padave <shaileshpadave@192.168.1.2>
2025-05-24 00:14:05 +0530

    Updated model file names

--- sdk/model/tag.go
+++ sdk/model/tag.go
@@ -5,3 +5,9 @@ type Tag struct {
 	Type_ string `json:"type,omitempty"`
 	Value string `json:"value,omitempty"`
 }
+
+type TagString struct {
+	Key   string `json:"key,omitempty"`
+	Type_ string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
