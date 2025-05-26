f0be7821062a083862667b704fd7762a47f5d9a0
Shailesh Padave <shaileshpadave49@gmail.com>
2025-05-26 15:33:54 +0530

    Update SDK POJO (github.com/conductor-sdk/conductor-go/sdk/model.event_handler) to match server implementation

--- sdk/model/event_handler.go
+++ sdk/model/event_handler.go
@@ -9,6 +9,7 @@
 
 package model
 
+// Defines an event handler
 type EventHandler struct {
 	Name          string   `json:"name"`
 	Event         string   `json:"event"`
@@ -16,4 +17,40 @@ type EventHandler struct {
 	Actions       []Action `json:"actions"`
 	Active        bool     `json:"active,omitempty"`
 	EvaluatorType string   `json:"evaluatorType,omitempty"`
+}
+
+type Action struct {
+	Action                 string                 `json:"action"`
+	StartWorkflow          *StartWorkflow         `json:"start_workflow,omitempty"`
+	CompleteTask           *TaskDetails           `json:"complete_task,omitempty"`
+	FailTask               *TaskDetails           `json:"fail_task,omitempty"`
+	ExpandInlineJSON       bool                   `json:"expandInlineJSON,omitempty"`
+	TerminateWorkflow      *TerminateWorkflow     `json:"terminate_workflow,omitempty"`
+	UpdateWorkflowVariables *UpdateWorkflowVariables `json:"update_workflow_variables,omitempty"`
+}
+
+type TaskDetails struct {
+	WorkflowId  string                 `json:"workflowId,omitempty"`
+	TaskRefName string                 `json:"taskRefName,omitempty"`
+	Output      map[string]interface{} `json:"output,omitempty"`
+	TaskId      string                 `json:"taskId,omitempty"`
+}
+
+type StartWorkflow struct {
+	Name         string                 `json:"name,omitempty"`
+	Version      int                    `json:"version,omitempty"`
+	CorrelationId string                `json:"correlationId,omitempty"`
+	Input        map[string]interface{} `json:"input,omitempty"`
+	TaskToDomain map[string]string      `json:"taskToDomain,omitempty"`
+}
+
+type TerminateWorkflow struct {
+	WorkflowId        string `json:"workflowId,omitempty"`
+	TerminationReason string `json:"terminationReason,omitempty"`
+}
+
+type UpdateWorkflowVariables struct {
+	WorkflowId string                 `json:"workflowId,omitempty"`
+	Variables  map[string]interface{} `json:"variables,omitempty"`
+	AppendArray *bool                 `json:"appendArray,omitempty"`
 }
