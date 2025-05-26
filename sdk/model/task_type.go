1b2f77ff851146e59dcff626efdb8cf21fbb4ef0
Shailesh Padave <shaileshpadave49@gmail.com>
2025-05-26 15:34:17 +0530

    Update SDK POJO (github.com/conductor-sdk/conductor-go/sdk/model.task_type) to match server implementation

--- sdk/model/task_type.go
+++ sdk/model/task_type.go
@@ -9,5 +9,33 @@
 
 package model
 
+// TaskType represents the type of task in the workflow
+const (
+	TaskTypeSimple           string = "SIMPLE"
+	TaskTypeDynamic          string = "DYNAMIC"
+	TaskTypeForkJoin         string = "FORK_JOIN"
+	TaskTypeForkJoinDynamic  string = "FORK_JOIN_DYNAMIC"
+	TaskTypeDecision         string = "DECISION"
+	TaskTypeSwitch           string = "SWITCH"
+	TaskTypeJoin             string = "JOIN"
+	TaskTypeDoWhile          string = "DO_WHILE"
+	TaskTypeSubWorkflow      string = "SUB_WORKFLOW"
+	TaskTypeStartWorkflow    string = "START_WORKFLOW"
+	TaskTypeEvent            string = "EVENT"
+	TaskTypeWait             string = "WAIT"
+	TaskTypeHuman            string = "HUMAN"
+	TaskTypeUserDefined      string = "USER_DEFINED"
+	TaskTypeHTTP             string = "HTTP"
+	TaskTypeLambda           string = "LAMBDA"
+	TaskTypeInline           string = "INLINE"
+	TaskTypeExclusiveJoin    string = "EXCLUSIVE_JOIN"
+	TaskTypeTerminate        string = "TERMINATE"
+	TaskTypeKafkaPublish     string = "KAFKA_PUBLISH"
+	TaskTypeJsonJQTransform  string = "JSON_JQ_TRANSFORM"
+	TaskTypeSetVariable      string = "SET_VARIABLE"
+	TaskTypeFork             string = "FORK"
+	TaskTypeNoop             string = "NOOP"
+)
+
 type TaskType struct {
 }
